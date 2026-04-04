ad7bda8fbf9f732ca5add669c506e311a8b27dc4
iro <i6iro@github.com>
2026-04-04 22:37:25 +0900

    BOJ/15963: attempt #1 - initial implement

---
+++ BOJ/15963/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long N, M;
+    cin >> N >> M;
+    if (N == M)
+        cout << 1;
+    else
+        cout << 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
