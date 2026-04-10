9eaeb29506d52e5d5387f5761f8865d1b9816551
Meleph <radionemu@konkuk.ac.kr>
2026-04-11 01:08:38 +0900

    BOJ/29751: attempt #1 - first implement

---
+++ BOJ/29751/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+
+void solve() {
+    float W, H;
+    cin >> W >> H;
+    cout << fixed << setprecision(1) << (W * H / 2.0);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
