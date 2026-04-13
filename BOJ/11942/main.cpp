06b6e3d93faea69a655ca2980ac3f6745f575b58
Meleph <radionemu@konkuk.ac.kr>
2026-04-13 21:21:07 +0900

    BOJ/11942: attempt #1 - first implement

---
+++ BOJ/11942/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    cout << "고려대학교";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
